977839eb97d3729cea65b32f76df785bfad75d9f
Benjamin Jacob Reji <benjaminjacobreji@gmail.com>
2022-01-18 16:29:55 +0400

    Fixed URL from localhost to hosted URL

--- js/part2.js
+++ js/part2.js
@@ -3,6 +3,25 @@ function logD3Version(){
     document.getElementById('version').innerHTML = d3.version;
 };
 
+function modifyParagraph(){
+    d3.select('p').style('font-size', '20px')
+        .style('line-height', '1.5em')
+        .style('color', '#FF0000')
+        .style('font-family', 'Arial');
+        // .text('This is a modified paragraph');
+}
+
+function addDivs(){
+    for (let i = 0; i < 10; i++) {
+        d3.select('body').append('div')
+            .attr('class', 'containers m-3')
+            .style('color', i < 5 ? 'red' : 'green')
+            .text(i + 1);
+    }
+}
+
 window.addEventListener('load', function() {
     logD3Version();
+    modifyParagraph();
+    addDivs();
 })
