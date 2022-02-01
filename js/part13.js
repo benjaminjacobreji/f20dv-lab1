4369722425babc91b7b6892298c8b317e1490b4a
Benjamin Jacob Reji <benjaminjacobreji@gmail.com>
2022-02-01 10:10:21 +0400

    Completed Part 13 of the Lab

---
+++ js/part13.js
@@ -0,0 +1,102 @@
+// Set Dimensions
+const xSize = 600; const ySize = 600;
+const margin = 40;
+const xMax = xSize - margin * 2;
+const yMax = ySize - margin * 2;
+
+function plotLineChart(file, color, shape) {
+
+    let data = [];
+
+    d3.csv(file, function (d) {
+        data.push({ x: d.x, y: parseFloat(d.y) });
+    }).then(function () {
+
+        // Get the 'limits' of the data - the full extent (mins and max)
+        // so the plotted data fits perfectly 
+        const xExtent = d3.extent(data, d => { return d.x });
+        const yExtent = d3.extent(data, d => { return d.y });
+        // Append SVG Object to the Page
+        const svg = d3.select("#body1")
+            .selectAll("svg")
+            .attr('width', xSize)
+            .attr('height', ySize)
+            .append("g")
+            .attr("transform", "translate(" + margin + "," + margin + ")");
+        // X Axis
+        const x = d3.scaleLinear()
+            .domain([xExtent[0], xExtent[1]])
+            .range([0, xMax]);
+        // bottom
+        svg.append("g")
+            .attr("transform", "translate(0," + yMax + ")")
+            .call(d3.axisBottom(x))
+            .attr('color', 'green'); // make bottom axis green
+        // top
+        svg.append("g")
+            .call(d3.axisTop(x));
+        // Y Axis
+        const y = d3.scaleLinear()
+            .domain([yExtent[0], yExtent[1]])
+            .range([yMax, 0]);
+        // left y axis
+        svg.append("g")
+            .call(d3.axisLeft(y));
+        // right y axis
+        svg.append("g")
+            .attr("transform", `translate(${yMax},0)`)
+            .call(d3.axisRight(y));
+        // Add the line
+        svg.append("path")
+            .datum(data)
+            .attr("fill", "none")
+            .attr("stroke", color)
+            .attr("stroke-width", 1.5)
+            .attr("d", d3.line()
+                .x(function (d) { return x(d.x) })
+                .y(function (d) { return y(d.y) })
+            );
+        // shapes
+        if (shape === 'circle') {
+            svg.selectAll("dot")
+            .data(data)
+            .enter()
+            .append("circle")
+            .attr("cx", function (d) { return x(d.x) })
+            .attr("cy", function (d) { return y(d.y) })
+            .attr("r", 5)
+            .style("fill", color);
+        }
+        if (shape === 'triangle') {
+            svg.selectAll("dot")
+            .data(data)
+            .enter()
+            .append("path")
+            .attr("d", d3.symbol().type(d3.symbolTriangle).size(100))
+            .style("fill", color)
+            .attr("transform", function (d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
+        }
+        // add the data points
+        svg.selectAll("points")
+            .data(data)
+            .enter()
+            .append("text")
+            .attr("x", function (d) { return x(d.x) - 30 })
+            .attr("y", function (d) { return y(d.y) })
+            .style("fill", color)
+            .text(function (d, i) { 
+                if (i % 10 == 0){
+                    return i;
+                }
+                return '';
+            });
+
+    });
+}
+
+window.addEventListener('load', function () {
+
+    plotLineChart('../csv/sine.csv', 'blue', 'circle');
+    plotLineChart('../csv/cosine.csv', 'green', 'triangle');
+
+})
